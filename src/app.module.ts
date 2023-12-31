7253c056a6eabb19d0f3bb3f15eb199429fcd615
quyendv <daovanquyen45@gmail.com>
2023-12-31 13:56:28 +0700

    feat: exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { CustomExceptionFilter } from './base/filters/exception.filter';
 import envConfig, { envValidation } from './configs/env.config';
 import { DatabaseModule } from './database/database.module';
 
@@ -23,6 +25,12 @@ const environment = process.env.NODE_ENV ?? 'local';
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: CustomExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
